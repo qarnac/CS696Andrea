4c07ba5ebe7d9041b9561ac86910d7309fb242c2
MOoMoOFarm <yansen85@gmail.com>
2013-06-11 00:56:56 -0700

    moved the sprite background to alien attack js

--- games/TheGame/AlienAttack.js
+++ games/TheGame/AlienAttack.js
@@ -7,3 +7,20 @@ var drawingSurface = canvas.getContext("2d");
 //Arrays to store the game objects and assets to load
 var sprites = [];
 var assetsToLoad = [];
+
+//Create the background
+var background = Object.create(spriteObject);
+background.x = 0;
+background.y = 0;
+background.sourceY = 32;
+background.sourceWidth = 480;
+background.sourceHeight = 320;
+background.width = 480;
+background.height = 320;
+sprites.push(background);
+
+//Create the cannon and center it
+var cannon = Object.create(spriteObject);
+cannon.x = canvas.width / 2 - cannon.width / 2;
+cannon.y = 280;
+sprites.push(cannon);
