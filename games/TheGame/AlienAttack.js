b49822ceee49733ac1c35cd08748c2d876d1c8ba
MOoMoOFarm <yansen85@gmail.com>
2013-06-11 00:50:24 -0700

    adding sprite obj js

---
+++ games/TheGame/AlienAttack.js
@@ -0,0 +1,9 @@
+//The canvas
+var canvas = document.querySelector("canvas");
+
+//Create the drawing surface 
+var drawingSurface = canvas.getContext("2d");
+
+//Arrays to store the game objects and assets to load
+var sprites = [];
+var assetsToLoad = [];
