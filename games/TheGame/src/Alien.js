fefa7bf66a3b7349397e6b35055859305fafe533
MOoMoOFarm <yansen85@gmail.com>
2013-06-18 01:23:54 -0700

    more refactoring in alien class

--- games/TheGame/src/Alien.js
+++ games/TheGame/src/Alien.js
@@ -1,6 +1,6 @@
 function Alien()
 {
-	this.alienObject = Object.create(spriteObject);
+	this.alienObject = new Entity();
 	this.alienObject.NORMAL = 1;
 	this.alienObject.EXPLODED = 2;
 	this.alienObject.state = alienObject.NORMAL;
