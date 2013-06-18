e0ef19a7bac87742bca92a9bc058c3702c16fcb7
MOoMoOFarm <yansen85@gmail.com>
2013-06-18 01:10:45 -0700

    re-refactoring game

--- games/TheGame/src/Game.js
+++ games/TheGame/src/Game.js
@@ -6,12 +6,6 @@ function Game()
 	this.alienFrequency = 100;
 	this.alienTimer = 0;
 	
-	//Arrays to store the game objects and assets to load
-	this.sprites = [];
-	this.assetsToLoad = [];
-	this.aliens = [];
-	this.messages = [];
-
 	//Variable to count the number of assets the game needs to load
 	this.assetsLoaded = 0;
 
@@ -25,20 +19,6 @@ function Game()
 	this.RIGHT = 39;
 	this.LEFT = 37;
 	this.SPACE = 32;
-
-	//Directions
-	this.moveRight = false;
-	this.moveLeft = false;
-
-	//Variables to help fire missiles
-	this.shoot = false;
-	this.spaceKeyIsDown = false;
-
-	//Game variables
-	this.score = 0;
-	this.scoreNeededToWin = 60;
-	this.alienFrequency = 100;
-	this.alienTimer = 0;
 
 }
 
