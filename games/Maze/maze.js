7e65b96dd2d8e5aeda68ae1ff0c13161143b996d
MOoMoOFarm <yansen85@gmail.com>
2013-06-19 23:37:33 -0700

    added build maps

--- games/Maze/maze.js
+++ games/Maze/maze.js
@@ -82,8 +82,227 @@ var assetsLoaded = 0;
 //Load the tilesheet image
 var image = new Image();
 image.addEventListener("load", loadHandler, false);
-image.src = "../images/timeBombPanic.png";
+image.src = "./images/timeBombPanic.png";
 assetsToLoad.push(image);
 
 //Game variables
 var bombsDefused = 0;
+
+//--- The gameWorld object
+var gameWorld = 
+{
+  x: 0,
+  y: 0,
+  width: map[0].length * SIZE,
+  height: map.length * SIZE,
+};
+
+//--- The camera object
+var camera = 
+{
+  x: 0,
+  y: 0,
+  width: canvas.width,
+  height: canvas.height,
+  
+  //The camera's inner scroll boundaries
+  rightInnerBoundary: function()
+  {
+    return this.x + (this.width / 2) + (this.width / 4);
+  },
+  leftInnerBoundary: function()
+  {
+    return this.x + (this.width / 2) - (this.width / 4);
+  },
+  topInnerBoundary: function()
+  {
+    return this.y + (this.height / 2) - (this.height / 4);
+  },
+  bottomInnerBoundary: function()
+  {
+    return this.y + (this.height / 2) + (this.height / 4);
+  }
+};
+
+//Center the camera over the gameWorld
+camera.x = (gameWorld.x + gameWorld.width / 2) - camera.width / 2;
+camera.y = (gameWorld.y + gameWorld.height / 2) - camera.height / 2;
+
+//Arrow key codes
+var UP = 38;
+var DOWN = 40;
+var RIGHT = 39;
+var LEFT = 37;
+
+//Directions
+var moveUp = false;
+var moveDown = false;
+var moveRight = false;
+var moveLeft = false;
+
+//Add keyboard listeners
+window.addEventListener("keydown", function(event)
+{
+  switch(event.keyCode)
+  {
+    case UP:
+	    moveUp = true;
+	    break;
+	  
+	  case DOWN:
+	    moveDown = true;
+	    break;
+	    
+	  case LEFT:
+	    moveLeft = true;
+	    break;  
+	    
+	  case RIGHT:
+	    moveRight = true;
+	    break; 
+  }
+}, false);
+
+window.addEventListener("keyup", function(event)
+{
+  switch(event.keyCode)
+  {
+    case UP:
+	    moveUp = false;
+	    break;
+	  
+	  case DOWN:
+	    moveDown = false;
+	    break;
+	    
+	  case LEFT:
+	    moveLeft = false;
+	    break;  
+	    
+	  case RIGHT:
+	    moveRight = false;
+	    break; 
+  }
+}, false);
+
+//Start the game animation loop
+update();
+
+function update()
+{ 
+  //The animation loop
+  requestAnimationFrame(update, canvas);
+  
+  //Change what the game is doing based on the game state
+  switch(gameState)
+  {
+    case LOADING:
+      console.log("loading...");
+      break;
+      
+    case BUILD_MAP:
+      buildMap(map);
+      buildMap(gameObjects);
+      createOtherObjects();
+      gameState = PLAYING;
+      break;
+    
+    case PLAYING:
+      playGame();
+      break;
+    
+    case OVER:
+      endGame();
+      break;
+  }
+  
+  //Render the game
+  render();
+}
+
+function loadHandler()
+{ 
+  assetsLoaded++;
+  if(assetsLoaded === assetsToLoad.length)
+  {
+    //Remove the load handlers
+    image.removeEventListener("load", loadHandler, false);
+        
+    //Build the map 
+    gameState = BUILD_MAP;
+  }
+}
+
+function buildMap(levelMap)
+{
+  for(var row = 0; row < ROWS; row++) 
+  {	
+    for(var column = 0; column < COLUMNS; column++) 
+    { 
+      var currentTile = levelMap[row][column];
+    
+      if(currentTile !== EMPTY)
+      {
+        //Find the tile's x and y position on the tile sheet
+        var tileSheetX = Math.floor((currentTile - 1) % tilesheetColumns) * SIZE; 
+        var tileSheetY = Math.floor((currentTile - 1) / tilesheetColumns) * SIZE;
+        
+        switch (currentTile)
+        {
+          case FLOOR:
+            var floor = Object.create(spriteObject);
+            floor.sourceX = tileSheetX;
+            floor.sourceY = tileSheetY;
+            floor.x = column * SIZE;
+            floor.y = row * SIZE;
+            sprites.push(floor);
+            break;
+          
+          case BOX:
+            var box = Object.create(spriteObject);
+            box.sourceX = tileSheetX;
+            box.sourceY = tileSheetY;
+            box.x = column * SIZE;
+            box.y = row * SIZE;
+            sprites.push(box);
+            boxes.push(box);
+            break;
+          
+          case WALL:
+            var wall = Object.create(spriteObject);
+            wall.sourceX = tileSheetX;
+            wall.sourceY = tileSheetY;           
+            wall.x = column * SIZE;
+            wall.y = row * SIZE;
+            sprites.push(wall);
+            break;
+          
+          case BOMB:
+            var bomb = Object.create(spriteObject);
+            bomb.sourceX = tileSheetX;
+            bomb.sourceY = tileSheetY; 
+            bomb.sourceWidth = 48;
+            bomb.sourceHeight = 36;
+            bomb.width = 48;  
+            bomb.height = 36;          
+            bomb.x = column * SIZE + 10;
+            bomb.y = row * SIZE + 16;
+            bombs.push(bomb);
+            sprites.push(bomb);
+            break;
+            
+          case ALIEN:
+            //Note: "alien" has already been defined in the main
+            //program so you don't neeed to preceed it with "var"
+            alien = Object.create(spriteObject);
+            alien.sourceX = tileSheetX;
+            alien.sourceY = tileSheetY;          
+            alien.x = column * SIZE;
+            alien.y = row * SIZE;
+            sprites.push(alien);
+            break;
+        }
+      }
+    }
+  }
+}
