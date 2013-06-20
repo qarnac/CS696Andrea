635a2e0548a1fe4d94c495f6200c94bfe0c1a0c5
MOoMoOFarm <yansen85@gmail.com>
2013-06-19 23:45:47 -0700

    added create other object, collission and end game

--- games/Maze/maze.js
+++ games/Maze/maze.js
@@ -306,3 +306,179 @@ function buildMap(levelMap)
     }
   }
 }
+
+function createOtherObjects()
+{
+  timeDisplay = Object.create(spriteObject);
+  timeDisplay.sourceX = 0;
+  timeDisplay.sourceY = 64;
+  timeDisplay.sourceWidth = 128;
+  timeDisplay.sourceHeight = 48;
+  timeDisplay.width = 128;  
+  timeDisplay.height = 48;            
+  timeDisplay.x = canvas.width / 2 - timeDisplay.width / 2;
+  timeDisplay.y = 8;
+  timeDisplay.scrollable = false;
+  sprites.push(timeDisplay);
+  
+  gameOverDisplay = Object.create(spriteObject);
+  gameOverDisplay.sourceX = 0;
+  gameOverDisplay.sourceY = 129;
+  gameOverDisplay.sourceWidth = 316;
+  gameOverDisplay.sourceHeight = 290;
+  gameOverDisplay.width = 316;  
+  gameOverDisplay.height = 290;            
+  gameOverDisplay.x = canvas.width / 2 - gameOverDisplay.width / 2;
+  gameOverDisplay.y = canvas.height / 2 - gameOverDisplay.height / 2;
+  gameOverDisplay.visible = false;
+  gameOverDisplay.scrollable = false;
+  sprites.push(gameOverDisplay);
+  
+  gameOverMessage = Object.create(messageObject);
+  gameOverMessage.x = 275;
+  gameOverMessage.y = 270;
+  gameOverMessage.font = "bold 30px Helvetica";
+  gameOverMessage.fillStyle = "black";
+  gameOverMessage.text = "";
+  gameOverMessage.visible = false;
+  messages.push(gameOverMessage);
+  
+  timerMessage = Object.create(messageObject);
+  timerMessage.x = 330;
+  timerMessage.y = 10;
+  timerMessage.font = "bold 40px Helvetica";
+  timerMessage.fillStyle = "white";
+  timerMessage.text = "";
+  messages.push(timerMessage);
+}
+
+function playGame()
+{ 
+  //Up
+  if(moveUp && !moveDown)
+  {
+    alien.vy = -4;
+  }
+  //Down
+  if(moveDown && !moveUp)
+  {
+    alien.vy = 4;
+  }
+  //Left
+  if(moveLeft && !moveRight)
+  {
+    alien.vx = -4;
+  }
+  //Right
+  if(moveRight && !moveLeft)
+  {
+    alien.vx = 4;
+  }
+
+  //Set the alien's velocity to zero if none of the keys are being pressed
+  if(!moveUp && !moveDown)
+  {
+    alien.vy = 0;
+  }
+  if(!moveLeft && !moveRight)
+  {
+    alien.vx = 0;
+  }
+  
+  //Move the alien and set its screen boundaries
+  alien.x = Math.max(64, Math.min(alien.x + alien.vx, gameWorld.width - alien.width - 64)); 
+  alien.y = Math.max(64, Math.min(alien.y + alien.vy, gameWorld.height - alien.height - 64));
+  
+  //Collisions with boxes
+  for(var i = 0; i < boxes.length; i++)
+  {
+    blockRectangle(alien, boxes[i]);
+  }
+    
+  //Collisions with bombs
+  for(var i = 0; i < bombs.length; i++)
+  {
+    var bomb = bombs[i];
+    
+    //If there's a collision, make the bombs invisible,
+    //reduce bombsDefused by 1, and check whether
+    //the player has won the game
+    if(hitTestCircle(alien, bomb) && bomb.visible)
+    {
+      bomb.visible = false;
+      bombsDefused++;
+      if(bombsDefused === bombs.length)
+      {
+        //Change the game state to OVER if the player has defused all the bombs
+        gameState = OVER;
+      }
+    }
+  }
+  
+  //Display the gameTimer.
+  //This modification adds an extra "0" to the time
+  //if the time is less than 10
+  timerMessage.text = gameTimer.time;
+  if(gameTimer.time < 10)
+  {
+    timerMessage.text = "0" + gameTimer.time;
+  }
+  
+  //Check whether the time is over
+  if(gameTimer.time === 0)
+  {
+    gameState = OVER;
+  }
+  
+  //Scroll the camera
+  if(alien.x < camera.leftInnerBoundary())
+  {
+    camera.x = Math.floor(alien.x - (camera.width / 4));
+  }
+  if(alien.y < camera.topInnerBoundary())
+  {
+    camera.y = Math.floor(alien.y - (camera.height / 4));
+  }
+  if(alien.x + alien.width > camera.rightInnerBoundary())
+  {
+    camera.x = Math.floor(alien.x + alien.width - (camera.width / 4 * 3));
+  }
+  if(alien.y + alien.height > camera.bottomInnerBoundary())
+  {
+    camera.y = Math.floor(alien.y + alien.height - (camera.height / 4 * 3));
+  }
+  
+  //The camera's gameWorld boundaries
+  if(camera.x < gameWorld.x)
+  {
+    camera.x = gameWorld.x;
+  }
+  if(camera.y < gameWorld.y)
+  {
+    camera.y = gameWorld.y;
+  }
+  if(camera.x + camera.width > gameWorld.x + gameWorld.width)
+  {
+    camera.x = gameWorld.x + gameWorld.width - camera.width;
+  }
+  if(camera.y + camera.height > gameWorld.height)
+  {
+    camera.y = gameWorld.height - camera.height;
+  } 
+}
+
+function endGame()
+{
+  gameTimer.stop();
+  gameOverDisplay.visible = true;
+  gameOverMessage.visible = true;
+    
+  if(bombsDefused === bombs.length)
+  {
+    gameOverMessage.text = "You Won!";
+  }
+  else
+  {
+    gameOverMessage.text = "You Lost!";
+  }
+}
