fb9be3089bd89308dcab0a1aeaa1cb5218321ba3
MOoMoOFarm <yansen85@gmail.com>
2013-06-30 23:42:06 -0700

    refactor game engine

--- games/Chapter11/game.js
+++ games/Chapter11/game.js
@@ -21,3 +21,125 @@ function Game() {
 	this.moveRight = false;
 	this.moveLeft = false;
 }
+
+Game.prototype.render = function(){
+
+  drawingSurface.clearRect(0, 0, canvas.width, canvas.height);
+  
+  //Display the game.sprites
+  if(this.sprites.length !== 0)
+  {
+	  for(var i = 0; i < this.sprites.length; i++)
+	  {
+		 var sprite = this.sprites[i];
+		 if(sprite.visible)
+		 {
+			 drawingSurface.drawImage
+			 (
+			   image, 
+			   sprite.sourceX, sprite.sourceY, 
+			   sprite.sourceWidth, sprite.sourceHeight,
+			   Math.floor(sprite.x), Math.floor(sprite.y), 
+			   sprite.width, sprite.height
+			 ); 
+		 }
+	  }
+  }
+
+};
+
+Game.prototype.changeGameStates = function(cat, canvas) {
+
+	  //Change what the game is doing based on the game state
+	  switch(this.gameState)
+	  {
+		case this.LOADING:
+		  console.log("loading...");
+		  break;
+		
+		case this.PLAYING:
+		  this.playGame(cat, canvas);
+		  break;
+	  }
+};
+
+Game.prototype.playGame = function(cat, canvas) {
+
+		//Set the cat's acceleration if the keys are being pressed
+		//Up
+		if(this.moveUp && !this.moveDown)
+		{
+			cat.accelerationY = -200.0;
+		}
+		//Down
+		if(this.moveDown && !this.moveUp)
+		{
+			cat.accelerationY = 200.0;
+		}
+		//Left
+		if(this.moveLeft && !this.moveRight)
+		{
+			cat.accelerationX = -0.2;
+		}
+		//Right
+		if(this.moveRight && !this.moveLeft)
+		{
+			cat.accelerationX = 0.2;
+		}
+
+		//Set the cat's velocity and acceleration to zero if none of the keys are being pressed
+		if(!this.moveUp && !this.moveDown)
+		{
+			cat.accelerationY = 0;
+			cat.vy = 0;
+		}
+		if(!this.moveLeft && !this.moveRight)
+		{
+			cat.accelerationX = 0;
+			cat.vx = 0;
+		}
+
+		//Apply the acceleration
+		cat.vx += cat.accelerationX; 
+		cat.vy += cat.accelerationY;
+
+		//Limit the speed
+		if (cat.vx > cat.speedLimit)
+		{
+			cat.vx = cat.speedLimit;
+		}
+		if (cat.vx < -cat.speedLimit)
+		{
+			cat.vx = -cat.speedLimit;
+		}
+		if (cat.vy > cat.speedLimit)
+		{
+			cat.vy = cat.speedLimit;
+		} 
+		if (cat.vy < -cat.speedLimit)
+		{
+			cat.vy = -cat.speedLimit;
+		}
+		
+		//Move the cat
+		cat.x += cat.vx;
+		cat.y += cat.vy;
+
+		//Screen boundaries
+		if (cat.x < 0)
+		{
+			cat.x = 0;
+		}
+		if (cat.y < 0)
+		{
+			cat.y = 0;
+		}
+		if (cat.x + cat.width > canvas.width)
+		{
+			cat.x = canvas.width - cat.width;
+		}
+		if (cat.y + cat.height > canvas.height)
+		{
+			cat.y = canvas.height - cat.height;
+		}
+};
