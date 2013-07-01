ee1dc3ac313bdc0b691d3895f873c9fd672e5d99
MOoMoOFarm <yansen85@gmail.com>
2013-06-30 22:48:19 -0700

    fixed Entity and added game,js

--- games/Chapter11/Entity.js
+++ games/Chapter11/Entity.js
@@ -1,28 +1,27 @@
 function Entity() {
+	this.sourceX= 0;
+	this.sourceY= 0;
+	this.sourceWidth= 64;
+	this.sourceHeight= 64;
+	this.width= 64;
+	this.height= 64;
+	this.x= 0;
+	this.y= 0;
+	this.vx= 0;
+	this.vy= 0;
+	this.visible= true;
 	
-		this.sourceX: 0;
-		this.sourceY: 0;
-		this.sourceWidth: 64;
-		this.sourceHeight: 64;
-		this.width: 64;
-		this.height: 64;
-		this.x: 0;
-		this.y: 0;
-		this.vx: 0;
-		this.vy: 0;
-		this.visible: true;
-		
-		//Physics properties
-		this.accelerationX: 0;
-		this.accelerationY: 0;
-		this.speedLimit: 5;
-		this.friction: 0.96;
-		this.bounce: -0.7;
-		this.gravity: 0.3;
-		
-		//Platform game properties
-		this.isOnGround: undefined;
-		this.jumpForce: -10;			
+	//Physics properties
+	this.accelerationX= 0;
+	this.accelerationY= 0;
+	this.speedLimit= 5;
+	this.friction= 0.96;
+	this.bounce= -0.7;
+	this.gravity= 0.3;
+	
+	//Platform game properties
+	this.isOnGround= undefined;
+	this.jumpForce= -10;			
 }
 
 
