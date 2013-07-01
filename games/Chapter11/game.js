dd60c263006f0420d06ca98808c36fc77d5f3ccd
MOoMoOFarm <yansen85@gmail.com>
2013-06-30 23:03:00 -0700

    added states and directions into game class

--- games/Chapter11/game.js
+++ games/Chapter11/game.js
@@ -3,4 +3,21 @@ function Game() {
 	this.sprites = [];
 	this.assetsToLoad = [];
 	this.assetsLoaded = 0;
+	
+	//Game states
+	this.LOADING = 0;
+	this.PLAYING = 1;
+	this.gameState = this.LOADING;
+	
+	//Arrow key codes
+	this.UP = 38;
+	this.DOWN = 40;
+	this.RIGHT = 39;
+	this.LEFT = 37;
+
+	//Directions
+	this.moveUp = false;
+	this.moveDown = false;
+	this.moveRight = false;
+	this.moveLeft = false;
 }
