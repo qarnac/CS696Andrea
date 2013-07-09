d3ff3aa4a7c1568e21c7fbf0ebc1631b55540af7
MOoMoOFarm <yansen85@gmail.com>
2013-07-08 22:36:54 -0700

    added missile fire

--- games/project/src/Alien.js
+++ games/project/src/Alien.js
@@ -11,3 +11,25 @@ Alien.prototype.update = function(){
 	this.sourceX = this.state * this.width;
 };
 
+Alien.prototype.fireMissile = function(sprites){
+  //Create a missile sprite
+  this.missile = new Entity();
+  this.missile.sourceX = 96;
+  this.missile.sourceWidth = 16;
+  this.missile.sourceHeight = 16;
+  this.missile.width = 16;
+  this.missile.height = 16;
+  
+  //Center it over the alien for release
+  this.missile.x = this.centerX() - this.missile.halfWidth();
+  this.missile.y = this.y - this.missile.height;
+  
+  //Set its speed
+  this.missile.vy = -8;
+  
+  //Push the missile into both the sprites and missiles arrays
+  sprites.push(this.missile);
+  this.missiles.push(this.missile);
+  
+  this.shoot = false;
+};
