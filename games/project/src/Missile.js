2a45dd39f77f724ec5c4743637bad20f76715a32
MOoMoOFarm <yansen85@gmail.com>
2013-07-23 01:13:39 -0700

    fixed missile formating

--- games/project/src/Missile.js
+++ games/project/src/Missile.js
@@ -1,11 +1,13 @@
 function Missile()
 {
+
   this.sourceX = 96;
   this.sourceY = 0;
   this.sourceWidth = 32;
   this.sourceHeight = 32;
   this.width = 16;
   this.height = 16;
+  
 }
 
 Missile.prototype = new Entity();
