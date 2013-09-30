db5f660ceee6885c341d48a8fdcf24c58ce55b73
MOoMoOFarm <yansen85@gmail.com>
2013-09-30 09:04:45 -0700

    added client database connection and jquery

--- Project/project/dbCyberScavenger.php
+++ Project/project/dbCyberScavenger.php
@@ -1,20 +1,22 @@
 <?php
 
-$host = "";
-$userName = "";
-$passwrd= "";
+$host = "68.178.216.51";
+$userName = "cyberread";
+$passwrd= "Carefusion101@";
 
 $databaseName = "cyberscavenger";
 $tablename = "hunt";
 
 	// Create connection
-	$con=mysqli_connect("68.178.216.51","cyberscavenger","Yenngsd101@","c");
+	$con=mysqli_connect($host,$userName,$passwrd,$databaseName);
 	
 	// Check connection
 	if (mysqli_connect_errno($con))
 	{
 	  //echo "Failed to connect to MySQL: " . mysqli_connect_error();
 	  echo "Failed to connect to MySQL please contact your administaror";
-	  
+	  print('connection failed');
 	}
+	
+	print('DB connecteed!');
 ?>
