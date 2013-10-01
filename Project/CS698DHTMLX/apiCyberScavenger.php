4331a8a7372a6a5a6bab57f6fd2103108c19f1f1
MOoMoOFarm <yansen85@gmail.com>
2013-09-30 22:26:09 -0700

    added changes for sql stmt

--- Project/CS698DHTMLX/apiCyberScavenger.php
+++ Project/CS698DHTMLX/apiCyberScavenger.php
@@ -6,17 +6,26 @@ $passwrd= "Carefusion101@";
 
 $databaseName = "cyberscavenger";
 $tablename = "hunt";
+$sqlStmt = "SELECT * FROM $tablename";
 
-	// Create connection
-	$con=mysqli_connect($host,$userName,$passwrd,$databaseName);
-	
-	// Check connection
-	if (mysqli_connect_errno($con))
-	{
-	  //echo "Failed to connect to MySQL: " . mysqli_connect_error();
-	  echo "Failed to connect to MySQL please contact your administaror";
-	  print('connection failed');
-	}
-	
-	print('DB connecteed!');
+
+// Create connection
+$con = mysqli_connect($host,$userName,$passwrd,$databaseName);
+
+// Check connection
+if (mysqli_connect_errno($con))
+{
+  //echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  echo "Failed to connect to MySQL please contact your administaror";
+  print('connection failed');
+}
+
+if ( $result = $con->query($sqlStmt) )
+{
+	/* associative array */
+	$row = $result->fetch_array(MYSQLI_ASSOC);
+	printf ("%s (%s)\n", $row["title"], $row["teacher_id"]);
+}
+
+	//print('DB connecteed!');
 ?>
