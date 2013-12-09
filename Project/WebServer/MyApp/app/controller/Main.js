8f7b5fc32de57a63c72a4da56a220c0bd3a76fdc
MOoMoOFarm <yansen85@gmail.com>
2013-12-08 21:22:16 -0800

    added changes for main

--- Project/WebServer/MyApp/app/controller/Main.js
+++ Project/WebServer/MyApp/app/controller/Main.js
@@ -20,10 +20,29 @@ Ext.define('myApp.controller.Main', {
         }
     },
 	
+	attachSecretMessage: function(map, marker, number) {
+		console.log("test");
+		var message = ["This","is","the","secret","message"];
+		var infowindow = new google.maps.InfoWindow(
+		{ 
+			content: message[number],
+			size: new google.maps.Size(50,50)
+		});
+			
+		google.maps.event.addListener(marker, 'click', function() 
+		{
+			console.log('clicked!');
+			//infowindow.open(map,marker);
+		});
+	},
+	
 	showPost: function(list, index, element, record){
 		console.log('Item tapped');
 		var me = this;
 		
+		//var infowindow = new google.maps.InfoWindow({
+		//	content: 'prova'
+		//});
 		this.getBlog().push({
 			xtype: 'map',
 			title: record.get('id'),
@@ -31,19 +50,25 @@ Ext.define('myApp.controller.Main', {
 			layout:'fit',
 			//added comment test
 			useCurrentLocation: false,
-			listeners: {
+			listeners: {				
 				maprender: function(comp, map) {
+					
 					var lat = parseFloat(record.get('latitude'));
 					var longi = parseFloat(record.get('longitude'));
 					var minLat = parseFloat(record.get('min_lat'));
 					var minLng = parseFloat(record.get('min_lng'));
 					var maxLat = parseFloat(record.get('max_lat'));
 					var maxLng = parseFloat(record.get('max_lng'));
+				
+//					var spanX   = (maxLat - minLat) * 1.1;
+//					var spanY   = (maxLng - minLng) * 1.1;
+					var centerX = (minLat + maxLat) / 2;
+					var centerY = (minLng + maxLng) / 2;
 					
-					console.log(minLat);
-					console.log(minLng);
-					console.log(maxLat);
-					console.log(maxLng);
+					var southWest = new google.maps.LatLng(minLat,minLng);
+					var northEast = new google.maps.LatLng(maxLat,maxLng);
+					var bounds = new google.maps.LatLngBounds(southWest,northEast);
+
 					
 					var position = new google.maps.LatLng(
 						lat, 
@@ -69,23 +94,19 @@ Ext.define('myApp.controller.Main', {
 						  new google.maps.LatLng(maxLat, maxLng))
 					});
 					
-					map.panTo(marker.getPosition());
-					map.setZoom(map.getZoom() - 5);
+					var panningPoint = new google.maps.LatLng(centerX, centerY);
 					
-                }
+				    map.fitBounds(bounds);
+					map.panTo(panningPoint);
+					map.setZoom(map.getZoom());
+					
+					attachSecretMessage(map, marker, 1);
+                },
+				
             }
 		
-			/*
-			items:[
-				{
-					xtype:'map',
-					useCurrentLocation:false,
-				}
-			]
-			*/
 		});
-		
-		
+			
 		//var map = Ext.ComponentQuery.query('map');
 /*
 		var map = new Ext.Map({
@@ -132,67 +153,8 @@ Ext.define('myApp.controller.Main', {
 		//); 
 	},
 	
-	/*
-		console.log('Item tapped');
-		var data = record.data;
-		var pos = new google.maps.LatLng(data.lat, data.long);
-        var zoomlevel = data.zoom;
-        var imgTiles = new google.maps.ImageMapType({
-			getTileUrl: function(coord, zoom) {
-				return data.url
-			},
-			tileSize: new google.maps.Size(256, 256),
-			isPng: true,
-			opacity: 1.0
-        });
-		console.log('Item tapped');
-		var mapView = Ext.create('Ext.Map', {
-            mapOptions : {
-                center : pos,
-                zoom : zoomlevel,
-                tilt: 0,
-                mapTypeId : google.maps.MapTypeId.ROADMAP,
-                panControl: false,
-                zoomControl: true,
-                zoomControlOptions: {
-                    position: google.maps.ControlPosition.TOP_LEFT,
-                    style: google.maps.ZoomControlStyle.SMALL
-                },
-                streetViewControl: false,
-                navigationControl: true,
-                navigationControlOptions: {
-                    style: google.maps.NavigationControlStyle.DEFAULT
-                }
-            },
-            listeners: {
-                delay: 500,
-                maprender: function(comp, map){                            
-                                map.overlayMapTypes.insertAt(0, imgTiles);
-                }
-            }
-        });
-		
-    
-        var mapPanel = Ext.create('Ext.Panel', {
-            fullscreen: true,
-            layout: 'fit',
-            items: [{
-                xtype: 'titlebar',
-                docked: 'top',
-                title: 'Map',
-                items: [{
-                    xtype: 'button',
-                    text: 'Back',
-                    ui: 'back',
-                    handler: function () {
-                            Ext.Viewport.setActiveItem(0, {type: 'slide', direction: 'right'});
-                        }
-                }]
-                },mapView]
-        });
-		
-        Ext.Viewport.animateActiveItem(mapPanel, {type: 'slide', direction: 'left'})
-	*/	
+
+	
 	
 
 });
