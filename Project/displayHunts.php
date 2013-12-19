09aab931b56794f91c16327bba7c3e36a12c58c8
MOoMoOFarm <yansen85@gmail.com>
2013-12-19 08:33:16 -0800

    added new query markers lat and longitudes

--- Project/displayHunts.php
+++ Project/displayHunts.php
@@ -1,32 +1,55 @@
 <?php
 header("Content-type: text/XML");
-
 require('cyberScavengerAPI.php');
+require('queryMarkersLatitudesLongitudes.php');
+
 
 $tablename = "hunt";
 $sqlStmt = "SELECT * FROM $tablename";//when query is sucessful
 
+
 if ( $result = $con->query($sqlStmt) )
 {
-	$xml = appendResultToXml($result);
+	$xml = appendResultToXml($result,$markersResult);
 	writeToXml($xml);
 }
 
-function appendResultToXml($result)
+function appendResultToXml($result, $markersResult)
 {
+	//load the string to xml objects
+	$xml = simplexml_load_string($markersResult);
+
 	$xmlResult = "<hunts>";
 	while ($row = $result->fetch_assoc()) 
 	{
+		//xpath
+		$resLat = $xml->xpath("/root/hunt_id[@id=".$row['id']."]/lat");
+		$resLng = $xml->xpath("/root/hunt_id[@id=".$row['id']."]/lng");
+		$latStr = "";
+		$lngStr = "";
+		
+
+		foreach ( $resLat as $index => $valueLat ) {
+			$latStr .= "<lat_lng>".$valueLat.", ".$resLng[$index]."</lat_lng>";	
+		}
+		
+
+		
+		
 		$xmlResult .= "<hunt>
 					  <id>".$row['id']."</id>
 					  <title>".$row['title']."</title><latitude>".$row['start_lat']."</latitude><longitude>".$row['start_lng']."</longitude>
 					  <min_lat>".$row['minlat']."</min_lat><min_lng>".$row['minlng']."</min_lng>
 					  <max_lat>".$row['maxlat']."</max_lat><max_lng>".$row['maxlng']."</max_lng>
-					  <question>".$row['additionalQuestions']."</question>
+					  <question>".$row['additionalQuestions']."</question>"
+					  .$latStr.$lngStr."
 					  </hunt>";
+				
 	}
 	
 	$xmlResult .= "</hunts>";
+
+	
 	return $xmlResult;
 }
 
