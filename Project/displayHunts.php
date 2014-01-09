15d97c3f14996aefb6feefa3415313aacf0cc1d9
MOoMoOFarm <yansen85@gmail.com>
2014-01-08 22:50:37 -0800

    added changes to the structure to support answers

--- Project/displayHunts.php
+++ Project/displayHunts.php
@@ -25,24 +25,25 @@ function appendResultToXml($result, $markersResult)
 		//xpath
 		$resLat = $xml->xpath("/root/hunt_id[@id=".$row['id']."]/lat");
 		$resLng = $xml->xpath("/root/hunt_id[@id=".$row['id']."]/lng");
-		$latLngStr = "<lat_lng>";
+		$mediaId = $xml->xpath("/root/hunt_id[@id=".$row['id']."]/media_id");
+		$additionalAnswers = $xml->xpath("/root/hunt_id[@id=".$row['id']."]/additionalAnswers");
+
+		$latLngStr = "";
 
 		foreach ( $resLat as $index => $valueLat ) {
-			$latLngStr .= $valueLat.",".$resLng[$index]."|";	
+			$latLngStr .= "<lat_lng>".$valueLat.",".$resLng[$index]."</lat_lng>";
+			$latLngStr .= "<mediaId>".$mediaId[$index]."</mediaId>";
+			$latLngStr .= "<additionalAnswers>".$additionalAnswers[$index]."</additionalAnswers>";
 		}
-		
-		
-		$latLngStr .= "</lat_lng>";
 
-		
 		
 		$xmlResult .= "<hunt>
 					  <id>".$row['id']."</id>
 					  <title>".$row['title']."</title><latitude>".$row['start_lat']."</latitude><longitude>".$row['start_lng']."</longitude>
 					  <min_lat>".$row['minlat']."</min_lat><min_lng>".$row['minlng']."</min_lng>
 					  <max_lat>".$row['maxlat']."</max_lat><max_lng>".$row['maxlng']."</max_lng>
-					  <question>".$row['additionalQuestions']."</question>"
-					  .$latLngStr."
+					  <question>".$row['additionalQuestions']."</question>
+					  <markers>".$latLngStr."</markers>
 					  </hunt>";
 				
 	}
