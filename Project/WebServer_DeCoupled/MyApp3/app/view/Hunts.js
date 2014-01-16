1361f7ad8df7e2e4fb0f54af29dc2db900de731d
MOoMoOFarm <yansen85@gmail.com>
2014-01-15 21:35:57 -0800

    fixed first page mvc

--- Project/WebServer_DeCoupled/MyApp3/app/view/Hunts.js
+++ Project/WebServer_DeCoupled/MyApp3/app/view/Hunts.js
@@ -15,35 +15,13 @@ Ext.define('myApp.view.Hunts', {
 		title:'Hunt',
 		iconCls: 'home',
 
-        // controllers:['Hunt'],
-        // store : 'myApp.store.HuntsStore',
 
         items:[{
 			xtype: 'list',
 			title: 'Scavenger Hunt',
 			itemTpl: '{title}',
 
-            //store: 'HuntsStore'
-            /*
-			store: {
-				autoLoad: true,
-				fields: ['id','title','latitude','longitude','min_lat','min_lng','max_lat','max_lng'],
-
-				//proxy tells how to load the data
-				proxy: {
-					type: 'ajax',
-					//force to use method get
-					useDefaultXhrHeader: false,
-					url: 'http://southsuco.com/MyApp/php/HomeDisplayHunt.php',
-
-                    reader: {
-                        type: 'xml',
-						contentType: "text/XML;",
-                        rootProperty: 'hunts',
-						record: 'hunt'
-					}
-				}
-			}*/
+            store: 'HuntsStore'
 		}]
 
 	}
