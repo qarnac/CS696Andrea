cb5b3f06dcb5487ca838bd5e2203453c6ee7a0fe
MOoMoOFarm <yansen85@gmail.com>
2014-07-16 01:14:06 -0700

    fixed github picture mapping

--- Project/WebServer_DeCoupled/MyApp3/src/php/SimpleImage.php
+++ Project/WebServer_DeCoupled/MyApp3/src/php/SimpleImage.php
@@ -6,21 +6,28 @@ require('./cyberScavengerAPIAdmin.php');
 
 	function insertImage($con, $imageFilename)
 	{
+		
 		$studentTableName = "image";
 		$sqlStmt = "INSERT INTO image(images) VALUES ('".$imageFilename."')";
 		$id = "";
+		
 
 		if ( $result = $con->query($sqlStmt) )
 		{
+			$last_id = 0;
 			//update the update to image name to ID name;
-			$sqlStmt = "UPDATE image SET images='". $con->insert_id .".jpg'". " WHERE id=" . $con->insert_id;
+			$sqlStmt = "UPDATE image SET images='". $con->insert_id .".jpg'". " WHERE id=" . $last_id = $con->insert_id;
 
 			$retval = $con->query($sqlStmt);
+		
 
 			if(! $retval )
 			{
 				die('Could not update data: ' . mysql_error());
 			}
+
+			
+			rename($imageFilename, "./../../images/".$last_id.".jpeg");
 
 		}
 
